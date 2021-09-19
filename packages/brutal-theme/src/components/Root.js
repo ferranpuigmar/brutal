aab49a36e59879dae9b2899956b4645b62f03b2f
Ferran <ferran.puigmar@gmail.com>
2021-09-19 16:53:42 +0200

    Merge from master

--- packages/brutal-theme/src/components/Root.js
+++ packages/brutal-theme/src/components/Root.js
@@ -30,9 +30,7 @@ const Main = styled.main`
 
 const Root = ( { state } ) =>
 {
-  console.log( "renderizo" )
   const data = state.source.get( state.router.link );
-  // console.log(`data`, data)
   const objPageIDs = Object.values( state.source.page ).find( page => page.link === data.link )
   const blackBackground = objPageIDs?.acf.footer_default_black;
 
